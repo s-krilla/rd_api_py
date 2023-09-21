38d8c5084d4f20a069f22b02eae0ad497e7153df
Damian Blankenship <damianblankenship@gmail.com>
2023-09-21 14:43:00 -0400

    Added python-dotenv as a required dependency

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='rd_api_py',
-   version='0.1',
+   version='0.1.1',
    description='Python library for Real Debrid API',
    author='$krilla',
    packages=find_packages(),
-   install_requires=[],
+   install_requires=['python-dotenv'],
 )
