eea17741a74eed76656a19329a965c2acc8957b4
Damian Blankenship <damianblankenship@gmail.com>
2023-09-22 13:30:59 -0400

    Modified package folder structure

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
    name='rd_api_py',
-   version='0.1.2',
+   version='0.1.3',
    description='Python library for Real Debrid API',
    author='$krilla',
-   packages=find_packages(),
+   package_dir={'': 'src'},
    install_requires=['python-dotenv'],
 )
