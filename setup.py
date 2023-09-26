aa5ccd9816ad9805cbccb420cf324ea7b9c86df4
Damian Blankenship <damianblankenship@gmail.com>
2023-09-25 22:26:00 -0400

    Incremented version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
    name='rd_api_py',
-   version='0.1.3',
+   version='0.1.4',
    description='Python library for Real Debrid API',
    author='$krilla',
    package_dir={'': 'src'},
